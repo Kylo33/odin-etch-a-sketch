7ec16c63519fbd2084da058be78c4a4b9b23ebeb
Kylo33 <56988649+Kylo33@users.noreply.github.com>
2024-09-03 10:50:26 -0600

    feat: add button to change the grid's size

--- script.js
+++ script.js
@@ -1,19 +1,31 @@
+const DEFAULT_GRID_SIZE = 16;
 const grid = document.querySelector("#grid");
 
-const size = 16;
+function drawGrid(size) {
+    grid.innerHTML = "";
+    const squareSize = grid.clientWidth / size;
 
-const squareSize = grid.clientWidth / size;
-
-for (let i = 0, n = size ** 2; i < n; i++) {
-    const box = document.createElement("div");
-    box.classList.add("box");
-    box.style.minWidth = `${squareSize}px`;
-    box.style.minHeight = `${squareSize}px`;
-    grid.appendChild(box);
+    for (let i = 0, n = size ** 2; i < n; i++) {
+        const box = document.createElement("div");
+        box.classList.add("box");
+        box.style.minWidth = `${squareSize}px`;
+        box.style.minHeight = `${squareSize}px`;
+        grid.appendChild(box);
+    }
 }
+
+document.querySelector("button").addEventListener("click", e => {
+    let input;
+    do {
+        input = parseInt(prompt("Enter a new grid size (1-100)"));
+    } while (!input || typeof input != "number" || Math.floor(input) != input || input < 1 || input > 100);
+    drawGrid(input)
+})
 
 document.addEventListener("mouseover", e => {
     if (e.target.classList.contains("box")) {
         e.target.style.backgroundColor = "black";
     }
 })
+
+drawGrid(DEFAULT_GRID_SIZE)
